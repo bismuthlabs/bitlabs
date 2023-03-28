9d756f2859e464d20987de50c19f6ae31fbff193
bismuthlabs <117520856+bismuthlabs@users.noreply.github.com>
2023-03-28 14:31:30 +0000

    robot moves... [without line trace]

--- components/program-codey.tsx
+++ components/program-codey.tsx
@@ -4,8 +4,10 @@ import { useEffect, useRef, useState } from 'react'
 import styles from './program-codey.module.css'
 
 function MyButton(): JSX.Element {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [numMoves, setNumMoves] = useState(1);
+  const [moves, setMoves] = useState(1);
+  const [selectedNumber, setSelectedNumber] = useState(1);
+  const [instructions, setInstructions] = useState('');
+  const canvasRef = useRef(null);
   const [robotPosition, setRobotPosition] = useState({ x: 300, y: 200 });
   const [isRunning, setIsRunning] = useState(false);
 
@@ -16,103 +18,114 @@ function MyButton(): JSX.Element {
       if (ctx) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
-        ctx.arc(robotPosition.x, robotPosition.y, 40, 0, 2 * Math.PI);
+        ctx.arc(robotPosition.x, robotPosition.y, 20, 0, 2 * Math.PI);
         ctx.fillStyle = 'yellow';
         ctx.fill();
         ctx.lineWidth = 3;
         ctx.strokeStyle = 'black';
         ctx.stroke();
         ctx.beginPath();
-        ctx.arc(robotPosition.x - 35, robotPosition.y - 20, 8, 0, 2 * Math.PI);
-        ctx.arc(robotPosition.x + 35, robotPosition.y - 20, 8, 0, 2 * Math.PI);
+        ctx.arc(robotPosition.x - 18, robotPosition.y - 10, 4, 0, 2 * Math.PI);
+        ctx.arc(robotPosition.x + 18, robotPosition.y - 10, 4, 0, 2 * Math.PI);
         ctx.fillStyle = 'black';
         ctx.fill();
         ctx.beginPath();
-        ctx.arc(robotPosition.x, robotPosition.y + 20, 12, Math.PI, 2 * Math.PI);
+        ctx.arc(robotPosition.x, robotPosition.y + 10, 6, Math.PI, 2 * Math.PI);
+        ctx.fill()
         ctx.lineWidth = 2;
         ctx.strokeStyle = 'black';
+        ctx.stroke();
+
+        // Draw line trace
+        ctx.beginPath();
+        ctx.moveTo(robotPosition.x, robotPosition.y);
+        ctx.lineTo(
+          robotPosition.x - 20 * Math.sin((2 * Math.PI * (moves - 1)) / 4),
+          robotPosition.y - 20 * Math.cos((2 * Math.PI * (moves - 1)) / 4)
+        );
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 2;
         ctx.stroke();
       }
     }
   }, [robotPosition, isRunning]);
 
-  const handleUpClick = () => {
-    console.log('Up button clicked!');
-    setRobotPosition((prevPos) => ({ x: prevPos.x, y: prevPos.y - 10 }));
+  const handleNumberSelect = (event) => {
+    setSelectedNumber(parseInt(event.target.value));
+    setMoves(event.target.value);
   };
 
-  const handleDownClick = () => {
-    console.log('Down button clicked!');
-    setRobotPosition((prevPos) => ({ x: prevPos.x, y: prevPos.y + 10 }));
-  };
-
-  const handleLeftClick = () => {
-    console.log('Left button clicked!');
-    setRobotPosition((prevPos) => ({ x: prevPos.x - 10, y: prevPos.y }));
-  };
-
-  const handleRightClick = () => {
-    console.log('Right button clicked!');
-    setRobotPosition((prevPos) => ({ x: prevPos.x + 10, y: prevPos.y }));
+  const handleButtonClick = (event) => {
+    const buttonName = event.target.innerText;
+    setInstructions(
+      (prevInstructions) => `${prevInstructions} ${moves} ${buttonName}`
+    );
   };
 
   const handleRunClick = () => {
-    console.log(`Run button clicked with numMoves=${numMoves}!`);
     setIsRunning(true);
-  };
-
-  const handleResetClick = () => {
-    console.log('Reset button clicked!');
-    setRobotPosition({ x: 300, y: 200 });
+    const instructionList = instructions.trim().split(' ');
+    let newPosition = { ...robotPosition };
+    for (let i = 0; i < instructionList.length; i += 2) {
+      const direction = instructionList[i + 1].toLowerCase();
+      const distance = parseInt(instructionList[i]);
+      switch (direction) {
+        case 'up':
+          newPosition.y -= distance;
+          break;
+        case 'down':
+          newPosition.y += distance;
+          break;
+        case 'left':
+          newPosition.x -= distance;
+          break;
+        case 'right':
+          newPosition.x += distance;
+          break;
+        default:
+          break;
+      }
+    }
+    setRobotPosition(newPosition);
     setIsRunning(false);
   };
 
-  const handleNumMovesChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setNumMoves(parseInt(event.target.value));
+  const handleResetClick = () => {
+    setRobotPosition({ x: 300, y: 200 });
+    setInstructions('');
+    setMoves(1);
   };
 
-  useEffect(() => {
-    if (isRunning) {
-      const interval = setInterval(() => {
-        setRobotPosition((prevPos) => ({ x: prevPos.x + 10, y: prevPos.y }));
-        setNumMoves((prevNumMoves) => prevNumMoves - 1);
-      }, 50);
-      return () => clearInterval(interval);
-    }
-  }, [isRunning]);
-
-  useEffect(() => {
-    if (numMoves === 0) {
-      setIsRunning(false);
-    }
-  }, [numMoves]);
-
   return (
-    <div className={styles.style}>
+    <div className={styles.style} style={{ position: 'relative' }}>
       <canvas ref={canvasRef} width={596} height={396} />
-      <div className={styles.style}>Your instructions will appear here.</div>
-      <div>
-        Move the robot{' '}
-        <select value={numMoves} onChange={handleNumMovesChange}>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={4}>4</option>
-          <option value={5}>5</option>
-        </select>{' '}
-        times.
+      <div className={styles.style}>
+        <div>
+          {instructions || 'Your instructions will appear here'}
+        </div>
+        <div>You moved {moves} times</div>
+        <div>
+          <select value={selectedNumber} onChange={handleNumberSelect}>
+            {[1, 2, 3, 4, 5].map((number) => (
+              <option key={number} value={number}>
+                {number}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
-      <div>
-        <button onClick={handleUpClick}>Up</button>
-      </div>
-      <div>
-        <button onClick={handleLeftClick}>Left</button>
-        <button onClick={handleDownClick}>Down</button>
-        <button onClick={handleRightClick}>Right</button>
-      </div>
-      <div>
-        <button onClick={handleRunClick}>RUN</button>
-        <button onClick={handleResetClick}>RESET</button>
+
+      <div className={styles.style}>
+        <div>
+          <button onClick={handleButtonClick}>Up</button>
+          <button onClick={handleButtonClick}>Down</button>
+          <button onClick={handleButtonClick}>Left</button>
+          <button onClick={handleButtonClick}>Right</button>
+        </div>
+        <div>
+          <button onClick={handleRunClick}>RUN</button>
+          <button onClick={handleResetClick}>RESET</button>
+        </div>
       </div>
     </div>
   );
